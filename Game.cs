3da1027dc6d58c820875fc4997498328534d1c4f
Sam Leong <85773134+Eclipixie@users.noreply.github.com>
2024-10-20 17:20:48 +1100

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,56 @@
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Volumetric {
+    public class Game : GameWindow {
+        public Game(int width, int height, string title) : 
+            base(GameWindowSettings.Default, new NativeWindowSettings() { 
+                ClientSize = (width, height), Title = title 
+            }) { }
+
+        readonly float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+            0.5f, -0.5f, 0.0f, //Bottom-right vertex
+            0.0f,  0.5f, 0.0f  //Top vertex
+        };
+
+        int VertexBufferObject;
+
+        protected override void OnUpdateFrame(FrameEventArgs args) {
+            base.OnUpdateFrame(args);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape)) {
+                Close();
+            }
+        }
+
+        protected override void OnLoad() {
+            base.OnLoad();
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            //Code goes here
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e) {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //Code goes here.
+
+            SwapBuffers();
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e) {
+            base.OnFramebufferResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Volumetric {
+    internal class Program {
+        static void Main() {
+            // See https://aka.ms/new-console-template for more information
+
+            using Game game = new(800, 600, "Volumetric");
+            game.Run();
+        }
+    }
+}
